b04220c0b8132ebf5a4f4342505a7dd55c4ca7d1
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-09-04 20:43:45 +0200

    Cant reproduce

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const { dispatch, state } = props;
+
+  React.useEffect(() => {
+    dispatch({ type: "any" });
+    dispatch({ type: "any" });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button type="button" onClick={() => dispatch({ type: "any" })}>
+        Inc
+      </button>
+      <p>clicked: {state}</p>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default connect(state => ({ state }))(App);
